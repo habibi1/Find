073f5fb5137ff167d69b88d45f3bbff0b2d3291e
Habibi <habibi.ilyas@gmail.com>
2022-01-22 19:03:28 +0700

    Initial commit

---
+++ app/src/main/java/com/habibi/find/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.habibi.find.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.habibi.find.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
